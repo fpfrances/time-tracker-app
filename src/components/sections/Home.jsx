2ef98e9a3db5d95af5dbf3edd285a695f5591258
Filipe <89738919+fpfrances@users.noreply.github.com>
2025-06-18 11:51:31 -0400

    fix: Correct punctuation in Home component description for clarity

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
         <div className="text-center z-10 px-4">
             <h1 className="masked-text text-3xl sm:text-6xl mb-3">Time Tracker App</h1>
             <p className="text-sm sm:text-xl mb-10 font-semibold bg-gradient-to-r from-purple-500 to-blue-600 bg-clip-text text-transparent leading-right">Track your time efficiently and effectively</p>
-            <p className="text-sm sm:text-xl font-semibold bg-gradient-to-r from-purple-500 to-blue-600 bg-clip-text text-transparent leading-right">Time Tracker puts you in charge of your workday without overcomplicated things. Just click start when you begin a task and stop when it's complete — easy as that. Regardless of whether you're working individually or with others, Time Tracker has it all in order so that you can get whatever you need done. No clutter, no confusion — just an easy method of tracking time and remaining productive.</p>
+            <p className="text-sm sm:text-xl font-semibold bg-gradient-to-r from-purple-500 to-blue-600 bg-clip-text text-transparent leading-right">Time Tracker puts you in charge of your workday without overcomplicated things. Just click start when you begin a task and stop when it's complete; It's that simple. Regardless of whether you're working individually or with others, Time Tracker has it all in order so that you can get whatever you need done. No clutter, no confusion, just an easy method of tracking time and remaining productive.</p>
             <div className="flex items-center justify-center space-x-4">
             <button
             onClick={() => {
